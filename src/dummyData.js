bdcfb4786251d79c7ca080a723cb51840837a22f
zigamak <zigamakinde@gmail.com>
2022-05-16 23:54:23 +0100

    Added the chart dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Jan',
+      uv: 4231,
+    },
+    {
+      name: 'Feb',
+      uv: 7030,
+    },
+    {
+      name: 'Mar',
+      uv: 2000,
+    },
+    {
+      name: 'Apr',
+      uv: 4000,
+    },
+    {
+      name: 'May',
+      uv: 4000,
+    },
+    {
+      name: 'Jun',
+      uv: 3000,
+    },
+    {
+      name: 'Jul',
+      uv: 4000,
+    },
+    {
+      name: 'Aug',
+      uv: 6000,
+    },
+    {
+      name: 'Sep',
+      uv: 4000,
+    },
+    {
+      name: 'Oct',
+      uv: 5000,
+    },
+    {
+      name: 'Nov',
+      uv: 4000,
+    },
+    {
+      name: 'Dec',
+      uv: 8000,
+    }
+    
+  ];
