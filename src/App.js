c040c523eeaa2099328185496e88fe00e945ebe8
zigamak <zigamakinde@gmail.com>
2022-05-17 21:27:29 +0100

    Improved the sidebar home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/sidebar";
-import Home from "./pages/home/home";
+import Topbar from "./components/topbar/Topbar";
 import "./App.css";
+import Home from "./pages/home/home";
 
 
 function App() {
   return (
-    <div>
-      <Topbar />
-      <div className="container" >
-        <Sidebar/>
-        <Home/>
-        
-      </div>
-    </div>
+    
+   <div className='App'>
+     <Topbar/>
+     
+    <div className='container'>
+     <Sidebar/>     
+     <Home/>
+     </div>
+   </div>
   );
 }
-
 export default App;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import "./home.css"
-import Charts from '../../components/charts/charts'
+import Charts from '../../components/charts/Charts'
 import Featuredinfo from '../../components/featuredinfo/Featuredinfo'
 import {userData} from '../../dummyData';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 export default function home() {
   return (
@@ -10,6 +12,8 @@ export default function home() {
         <Featuredinfo/>
         <Charts data={userData} title="User Analytics" grid dataKey="Active User"/>
         <div className="homeWidgets"></div>
+        <WidgetSm />
+        <WidgetLg />
     </div>
   )
 }
