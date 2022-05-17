7e3ec3d2e290984ebe5833547265174225fc1de0
zigamak <zigamakinde@gmail.com>
2022-05-18 00:14:59 +0100

    Added the page for new products and styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import Sidebar from "./components/sidebar/sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/home";
+import { BrowserRouter as Router, BrowserRouter as Routes, Route } from "react-router-dom";
 
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    
-   <div className='App'>
-     <Topbar/>
-     
-    <div className='container'>
-     <Sidebar/>     
-     <Home/>
-     </div>
-   </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,27 +1,33 @@
 import "./sidebar.css";
-import {LineStyle,
-  TrendingUp, 
-  Timeline, 
-  Person, 
-  Category,
-   AttachMoney, 
-   Mail, 
-   Feedback, 
-   Message, 
-   AccountBox,
-  Error  } from "@material-ui/icons";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
+            <Link to="/" className="link">
             <li className="sidebarListItem active">
               <LineStyle className="sidebarIcon" />
               Home
             </li>
+            </Link>
             <li className="sidebarListItem">
               <Timeline className="sidebarIcon" />
               Analytics
@@ -30,50 +36,55 @@ export default function Sidebar() {
               <TrendingUp className="sidebarIcon" />
               Sales
             </li>
-            
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem ">
-              <Person className="sidebarIcon" />
-              User
-            </li>
-            <li className="sidebarListItem">
-              <Category className="sidebarIcon" />
-              Products
-            </li>
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <Storefront className="sidebarIcon" />
+                Products
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <AttachMoney className="sidebarIcon" />
-              Transaction
+              Transactions
             </li>
-            
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Reports
+            </li>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Notifications</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem ">
-              <Mail className="sidebarIcon" />
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
               Mail
             </li>
             <li className="sidebarListItem">
-              <Feedback className="sidebarIcon" />
+              <DynamicFeed className="sidebarIcon" />
               Feedback
             </li>
             <li className="sidebarListItem">
-              <Message className="sidebarIcon" />
+              <ChatBubbleOutline className="sidebarIcon" />
               Messages
             </li>
-            
           </ul>
         </div>
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Team Members</h3>
+          <h3 className="sidebarTitle">Staff</h3>
           <ul className="sidebarList">
             <li className="sidebarListItem">
-              <AccountBox className="sidebarIcon" />
+              <WorkOutline className="sidebarIcon" />
               Manage
             </li>
             <li className="sidebarListItem">
@@ -81,14 +92,12 @@ export default function Sidebar() {
               Analytics
             </li>
             <li className="sidebarListItem">
-              <Error className="sidebarIcon" />
+              <Report className="sidebarIcon" />
               Reports
             </li>
-            
           </ul>
         </div>
       </div>
-      
     </div>
   );
 }
